e0bf41f33539a382b49159f65ee2dffd6b100b75
M <M@qq.com>
2019-04-01 01:46:15 +0800

    init commit

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-cache.put("frontendData", { "nodes": [{ "blockId": "decisioncontainer1", "nodeType": "decision", "positionX": 1130, "positionY": 267 }, { "blockId": "taskcontainer2", "nodeType": "task", "positionX": 729, "positionY": 170 }, { "blockId": "decisioncontainer3", "nodeType": "decision", "positionX": 745, "positionY": 401 }, { "blockId": "taskcontainer4", "nodeType": "task", "positionX": 328, "positionY": 368 }, { "blockId": "decision234759", "nodeType": "task", "positionX": 411, "positionY": 140 }], "connections": [{ "uuids": ["decision234759rm-out", "taskcontainer2lm-in"] }, { "uuids": ["taskcontainer4rm-out", "decisioncontainer3ll-in"] }, { "uuids": ["decisioncontainer3rm-out", "decisioncontainer1lr-in"] }], "numberOfElements": 5 });
+cache.put("frontendData", { "nodes": [{ "blockId": "decision585016", "nodeType": "task", "positionX": 462, "positionY": 177 }, { "blockId": "task585832", "nodeType": "task", "positionX": 198, "positionY": 164 }], "connections": [{ "uuids": ["task585832rm-out", "decision585016lm-in"] }], "numberOfElements": 2 });
 
-app.get('/update-task-info', function (req, res, next) {
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post('/update-task-info', function (req, res, next) {
   let frontendData;
   Object.keys(req.body).forEach(key => {
     cache.put(key, req.body[key])
   });
-
 
   res.statusCode = 200;
 
@@ -36,9 +41,9 @@ app.get('/update-task-info', function (req, res, next) {
 app.get("/get-task-info", function (req, res, next) {
   const frontendData = cache.get("frontendData");
 
-  // console.log(cache.get("frontendData"));
+  console.log(cache.get("frontendData"));
 
-  res.statusCode = 200;
+  // res.statusCode = 200;
 
   res.json({
     statusCode: 200,
@@ -50,5 +55,5 @@ app.get("/get-task-info", function (req, res, next) {
 });
 
 app.listen(3000, function () {
-  console.log("http://localhost:3000")
+  console.log("http://localhost:3000");
 });
